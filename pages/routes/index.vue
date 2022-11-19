9f435c68657e634211772abcad13ce0731225440
Patrick Schilf <pschilf@unb.ca>
2022-11-19 13:49:37 -0400

    Fix "Routes" page title

--- pages/routes/index.vue
+++ pages/routes/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <PageSection :title="Routes">
+  <PageSection class="mt-40" :title="'Routes'">
     <div class="mb-40 grid grid-cols-3 gap-6">
       <Teaser v-for="route in routes" :key="route.id" class="aspect-4/3" :route="route" />
     </div>
