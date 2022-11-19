f0836fa2c4020765064eeabe1f4d4f8db7a4f877
Patrick Schilf <pschilf@unb.ca>
2022-11-19 00:13:01 -0400

    Create "routes" page

---
+++ pages/routes/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <PageSection :title="Routes">
+    <div class="mb-40 grid grid-cols-3 gap-6">
+      <Teaser v-for="route in routes" :key="route.id" class="aspect-4/3" :route="route" />
+    </div>
+  </PageSection>
+</template>
+
+<script setup lang="ts">
+  const routes = await useRoutes().all()
+</script>
