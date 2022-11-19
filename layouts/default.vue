e185423a967040688ebc7b15944dfd6613517a13
Patrick Schilf <pschilf@unb.ca>
2022-11-19 00:08:24 -0400

    Keep footer at bottom of window

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,31 @@
 <template>
-  <header>
-    <div class="bg-yellow p-4">
-      <div class="container mx-auto flex flex-row justify-between text-lg">
-        <div>
-          <img src="/img/btnb_logo.png" alt="BikeTourNB Logo" />
+  <div class="flex flex-col h-full">
+    <header class="flex-none">
+      <div class="bg-yellow p-4">
+        <div class="container mx-auto flex flex-row justify-between text-lg">
+          <div>
+            <img src="/img/btnb_logo.png" alt="BikeTourNB Logo" />
+          </div>
+          <nav class="flex items-center space-x-6">
+            <a href="#">Routes</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+          </nav>
         </div>
+      </div>
+    </header>
+    <main class="flex-grow">
+      <slot />
+    </main>
+    <footer class="flex-none bg-gray-200 p-4">
+      <div class="container mx-auto flex flex-row">
+        <img class="mr-6 h-8" src="/img/btnb_logo_bw.png" alt="BikeTourNB Logo" />
         <nav class="flex items-center space-x-6">
-          <a href="#">Routes</a>
-          <a href="#">About</a>
+          <a href="#">Copyright</a>
+          <a href="#">Disclaimer</a>
           <a href="#">Contact</a>
         </nav>
       </div>
-    </div>
-  </header>
-  <main>
-    <slot />
-  </main>
-  <footer class="bg-gray-200 p-4">
-    <div class="container mx-auto flex flex-row">
-      <img class="mr-6 h-8" src="/img/btnb_logo_bw.png" alt="BikeTourNB Logo" />
-      <nav class="flex items-center space-x-6">
-        <a href="#">Copyright</a>
-        <a href="#">Disclaimer</a>
-        <a href="#">Contact</a>
-      </nav>
-    </div>
-  </footer>
+    </footer>
+  </div>
 </template>
