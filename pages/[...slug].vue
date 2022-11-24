44ae969718639d916dda5f292f8cf19cd1a720f0
Patrick Schilf <pschilf@unb.ca>
2022-11-24 17:24:19 -0400

    Add page header to static pages

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,3 +1,8 @@
 <template>
-  <ContentDoc />
+  <ContentDoc v-slot="{ doc: page }">
+    <PageHeader :title="page.title" />
+    <div class="container mx-auto text-lg">
+      <ContentRenderer :value="page"/>
+    </div>
+  </ContentDoc>
 </template>
