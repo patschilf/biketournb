5e0a5ae35bc2cce7e4b6815fe9346217fbc8fdfc
Patrick Schilf <pschilf@unb.ca>
2022-12-03 19:27:48 -0400

    Modal to enlarge gallery images

--- pages/routes/nb-[id].vue
+++ pages/routes/nb-[id].vue
@@ -12,9 +12,29 @@
       </PageSection>
       <PageSection v-if="route.images" title="Sneak Peak">
         <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-          <nuxt-img v-for="image of route.images" :src="image" loading="lazy" sizes="xs:320px sm:640px md:384px lg:324px xl:427px 2xl:512px" />
+          <nuxt-img v-for="image of route.images" class="hover:cursor-pointer"
+            :src="image" 
+            loading="lazy" 
+            sizes="xs:320px sm:640px md:384px lg:324px xl:427px 2xl:512px" 
+            @click.prevent="(zoomOnImage = image)" />
+          <Modal v-if="zoomOnImage" @modal:close="zoomOnImage = ''">
+            <nuxt-img 
+              class="mb-4 aspect-4/3 h-full" 
+              sizes="xs:320px sm:640px md:768px lg:1024px xl:1280px" 
+              :src="zoomOnImage" />
+          </Modal>
         </div>
       </PageSection>
     </div>
   </ContentDoc>
 </template>
+
+<script setup lang="ts">
+  const zoomOnImage = ref("")
+</script>
+
+<style scoped>
+  img {
+    max-height: calc(90vh - 2rem);
+  }
+</style>
