e1e30b10529f938b3b2dff33cdfe41e42de90e73
Patrick Schilf <pschilf@unb.ca>
2022-12-03 19:24:39 -0400

    Make modal responsive

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <Teleport to="body">
-    <div class="top-0 left-0 w-full h-full overflow-hidden backdrop-brightness-50 backdrop-blur-sm fixed flex items-center z-50" @click.self="$emit('modal:close')">
-      <div class="p-4 h-4/5 relative flex flex-col mx-auto opacity-100 bg-white">
+    <div id="modal" class="top-0 left-0 w-full h-full backdrop-brightness-50 backdrop-blur-sm fixed flex items-center" @click.self="$emit('modal:close')">
+      <div id="mbody" class="p-4 relative flex flex-col mx-auto opacity-100 bg-white">
         <slot />
       </div>
     </div>
@@ -11,3 +11,10 @@
 <script setup lang="ts">
   defineEmits(['modal:close'])
 </script>
+
+<style scoped>
+  #mbody {
+    max-height: 90vh;
+    max-width: 90vw;
+  }
+</style>
