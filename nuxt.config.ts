9648a7bf67824973e7ac10fe12cd8ac111d7f864
Patrick Schilf <pschilf@unb.ca>
2023-01-02 13:46:48 -0400

    Remove generating IPX routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,5 @@
 const { theme } = require('./tailwind.config')
-const glob = require('glob')
 
-const ipxRoutes = (() => {
-  return glob.sync('/img/**/*.jpg', {nodir: true, root: 'public', nomount: true})
-    .map((filename: string) => [150, 320, 480, 640, 768, 1024, 1280, 1600]
-      .map((width: number) => `/_ipx/w_${width}${filename}`))
-    .reduce((all: string[], routes: string[]) => all.concat(...routes), [])
-})()
 const { CLOUDINARY_PROJECT_ID } = process.env
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
@@ -32,13 +25,6 @@ export default defineNuxtConfig({
     '@nuxt/content',
     '@nuxt/image-edge',
   ],
-  nitro: {
-    preset: "vercel",
-    prerender: {
-      crawlLinks: true,
-      routes: ['/', ...ipxRoutes],
-    },
-  },
   postcss: {
     plugins: {
       tailwindcss: {},
