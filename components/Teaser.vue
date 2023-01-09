91a5aa48c3c20deeac539bea5f4b72d07054da00
Patrick Schilf <pschilf@unb.ca>
2023-01-09 08:51:04 -0400

    Set teaser hover style

--- components/Teaser.vue
+++ components/Teaser.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="group flex items-end bg-gray-300">
-    <NuxtLink :to="url" class="w-full text-white group-hover:text-black bg-gradient-to-b from-transparent to-black group-hover:to-yellow p-4 pt-16 group-hover:pt-4 group-hover:h-full flex flex-col">
+  <div class="group flex items-end bg-gray-300" :style="style">
+    <NuxtLink :to="url" class="w-full text-white group-hover:text-black bg-gradient-to-b from-transparent to-black group-hover:from-yellow/90 group-hover:to-yellow/90 group-hover:bg p-4 pt-16 group-hover:pt-4 group-hover:h-full flex flex-col">
       <h3 class="text-xl font-bold hover:text-red">{{ route.title }}</h3>
       <div class="hidden group-hover:block flex-grow">
         <slot />
