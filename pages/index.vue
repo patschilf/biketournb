763ec9a0cbd52e0e35719f458187ca4ae6285e1b
Patrick Schilf <pschilf@unb.ca>
2023-01-31 19:19:51 -0400

    Shrink map container

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-36 bg-red z-10"></div>
-  <LeafletMap class="container mx-auto w-full h-screen -mt-12"
+  <LeafletMap class="container mx-auto aspect-3/2 -mt-12"
     :view="[46.5, -66.0]" 
     :zoom="8" 
     :max-zoom="18"
