31409d4459b7bb8513c147c43bf5e6d4c5e023e6
Patrick Schilf <pschilf@unb.ca>
2023-03-05 01:34:37 -0400

    Refactor route index view

--- pages/routes/index.vue
+++ pages/routes/index.vue
@@ -12,14 +12,14 @@
         <div class="flex flex-col sm:flex-row flex-initial gap-4">
           <FormFilterRange class="w-full sm:w-1/2 min-w-60"
             name="distance"
-            :label="distanceLabel"
+            :label="`Max. Distance: ${distance < maxDistance ? `${distance}km` : 'any'}`"
             v-model="distance"
             :min="minDistance"
             :max="maxDistance"
             step="10" />
           <FormFilterRange class="w-full sm:w-1/2 min-w-60"
             name="elevation"
-            :label="elevationLabel"
+            :label="`Max. Elevation: ${elevation < maxElevation ? `${elevation}m` : 'any'}`"
             v-model="elevation"
             :min="minElevation"
             :max="maxElevation"
@@ -28,7 +28,7 @@
       </div>
     </form>
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-      <Teaser class="aspect-4/3" v-for="route of routes" :key="route._path" :route="route" :url="route._path">
+      <Teaser class="aspect-4/3" v-for="route of routes.filter(routeFilter)" :key="route._path" :route="route" :url="route._path">
         <ContentRenderer :value="route" :excerpt="true" />
       </Teaser>
     </div>
@@ -36,27 +36,23 @@
 </template>
 
 <script setup lang="ts">
-  const routeContent = await queryRoutes()
-  const locations = unique(routeContent.map(route => route.location))
+  import { ParsedContent } from '@nuxt/content/dist/runtime/types';
+
+  const routes = await queryRoutes()
+  const locations = unique(routes.map(route => route.location))
   const location = ref("")
+
+  function routeFilter(route: ParsedContent) {
+    return (location.value === "" || route.location.id == location.value) 
+      && (distance.value === maxDistance || route.distance <= distance.value )
+      && (elevation.value === maxElevation || route.elevation <= elevation.value)
+  }
 
   const minDistance = 20
   const maxDistance = 120
   const distance = ref(120)
-  const distanceLabel = computed(() => distance.value < maxDistance
-    ? `Max. Distance: ${distance.value}km`
-    : 'Distance: any')
 
   const minElevation = 100
   const maxElevation = 1000
   const elevation = ref(1000)
-  const elevationLabel = computed(() => elevation.value < maxElevation
-    ? `Max. Elevation: ${elevation.value}m`
-    : 'Elevation: any')
-
-  const routes = computed(() => routeContent.filter(route => 
-    (location.value === "" || route.location.id == location.value) 
-      && (distance.value === maxDistance || route.distance <= distance.value )
-      && (elevation.value === maxElevation || route.elevation <= elevation.value)))
-    
 </script>
