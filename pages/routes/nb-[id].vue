741f1cbb2529bd1e8fd97f58e63aac7915653be3
Patrick Schilf <pschilf@unb.ca>
2023-03-05 22:15:50 -0400

    Fix gallery navigation

--- pages/routes/nb-[id].vue
+++ pages/routes/nb-[id].vue
@@ -16,19 +16,19 @@
             :src="image" 
             loading="lazy" 
             sizes="xs:320px sm:640px md:384px lg:324px xl:427px 2xl:512px" 
-            @click.prevent="gallery.open = true; gallery.index = index" />
-          <Modal v-if="gallery.open" @modal:close="gallery.open = false">
+            @click.prevent="showGallery = true; galleryIndex = index" />
+          <Modal v-if="showGallery" @modal:close="showGallery = false">
             <nuxt-img 
               class="mb-4 aspect-4/3 h-full" 
               sizes="xs:320px sm:640px md:768px lg:1024px xl:1280px" 
-              :src="route.images.gallery.at(gallery.index)" />
+              :src="route.images.gallery.at(galleryIndex % route.images.gallery.length)" />
             <div class="static">
-              <button class="absolute -left-6 top-1/2 p-4 bg-white rounded-full" @click.prevent="gallery.index = --gallery.index % route.images.length">
+              <button class="absolute -left-6 top-1/2 p-4 bg-white rounded-full" @click.prevent="--galleryIndex">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
                 </svg>
               </button>
-              <button class="absolute -right-6 top-1/2 p-4 bg-white rounded-full" @click.prevent="gallery.index = ++gallery.index % route.images.length">
+              <button class="absolute -right-6 top-1/2 p-4 bg-white rounded-full" @click.prevent="++galleryIndex">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
                 </svg>
@@ -42,10 +42,8 @@
 </template>
 
 <script setup lang="ts">
-  const gallery = reactive({
-    open: false,
-    index: 0,
-  })
+  const showGallery = ref(false)
+  const galleryIndex = ref(0)
 </script>
 
 <style scoped>
