7c3d82cd28fc7d9d89fc67a59c4a412a25c878f2
Patrick Schilf <pschilf@unb.ca>
2023-03-12 19:19:06 -0300

    Stretch front page map to fill width,height

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="h-36 bg-red z-10"></div>
-  <LeafletMap class="container mx-auto aspect-3/2 -mt-12"
+  <div class="h-6 bg-red z-10"></div>
+  <LeafletMap class="h-screen"
     :view="[46.5, -66.0]" 
     :zoom="8" 
     :max-zoom="18"
