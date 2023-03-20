56336426d930247769ae6f91721d349b7b67e995
Patrick Schilf <pschilf@unb.ca>
2023-03-19 22:14:53 -0300

    Footer text color (mobile)

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,7 +26,7 @@
     <footer class="flex-none bg-gray-200 p-4">
       <div class="container mx-auto flex flex-col md:flex-row items-center">
         <nuxt-img class="w-32 sm:w-40 md:mr-6 md:h-8" src="/misc/biketournb_logo_bw.png" alt="BikeTourNB Logo" sizes="xs:120px sm:160px" />
-        <nav class="flex flex-col md:flex-row items-center md:space-x-6 mt-4 md:mt-0">
+        <nav class="flex flex-col md:flex-row items-center md:space-x-6 mt-4 md:mt-0 text-gray-600 md:text-black">
           <NuxtLink to="/copyright">Copyright</NuxtLink>
           <NuxtLink to="/disclaimer">Disclaimer</NuxtLink>
           <NuxtLink to="/contact">Contact</NuxtLink>
